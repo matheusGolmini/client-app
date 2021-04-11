2119d5e05659521a832e991e3c88951a9165c101
Matheus <matheus>
2021-04-11 10:43:30 -0300

    feat: add footer

---
+++ src/components/footer/styles.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    footer:{
+        position: 'absolute',
+        flex:0.1,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height:60,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: '#A9A9A9',
+    },
+    power:{
+        alignItems: 'center'
+    },
+    button: {
+        flex:1,
+        backgroundColor: 'transparent',
+        marginLeft: 5,
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    iconText: {
+        fontSize: 20,
+        color: "white",
+        fontWeight:"bold",
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    
+});
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Footer from '../../components/footer'
 
 const Home = () => {
     return (
-
-        <Text style={{color: 'black'}}> Home </Text>
-
+        <>
+            <Text style={{color: 'black'}}> Home </Text>
+            <Footer />
+        </>
     )
 }
 
