2db32728b5404a9e31bed2312ff0e825e91ce7c3
Matheus <matheus>
2021-04-25 19:03:55 -0300

    refactor: remove interface not used

--- src/interfaces/service.tsx
+++ src/interfaces/service.tsx
@@ -1,14 +1,3 @@
-export interface IService {
-    id: string;
-    name: string;
-    end_date: Date;
-    start_date: Date;
-    title: string;
-    image: string;
-    color: string;
-    status: string;
-}
-
 export interface DetailService {
     id: string
     service: string;
